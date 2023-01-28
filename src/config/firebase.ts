1af3fcf3ddecaa56a36f4e07610447dfac908e4b
shamal-iroshan <shamaliroshan34@gmail.com>
2023-01-28 17:36:17 +0530

    changed credential to env

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,21 +1,13 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_API_KEY,
-//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGING_ID,
-//   appId: process.env.REACT_APP_APP_ID,
-// };
 const firebaseConfig = {
-  apiKey: 'AIzaSyAzPvVjcSm-Pq2jkHRv7Jh9UoK23P3J8lQ',
-  authDomain: 'shamaliroshan-com.firebaseapp.com',
-  projectId: 'shamaliroshan-com',
-  storageBucket: 'shamaliroshan-com.appspot.com',
-  messagingSenderId: '955758680872',
-  appId: '1:955758680872:web:9c8e27187735cdb7f54e1d',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
